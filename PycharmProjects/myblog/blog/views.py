aee38af09fba218fc1b85a5cd79c35eeda29bf7e
WINSEN <791417205@qq.com>
2017-06-26 04:11:06 +0800

    update app blog 1.0.1

--- PycharmProjects/myblog/blog/views.py
+++ PycharmProjects/myblog/blog/views.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from  django.http import  HttpResponse
+from django.http import  HttpResponse
 
 
 def index(request):
-    return HttpResponse('hello')
+    #return HttpResponse('hello')
+    return render(request,'blog/index.html',{'data':'hello,blog'})
